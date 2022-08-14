7199e5b4c1221c9850603ffdeab0df65bc79419f
arkadiyshin <108717969+arkadiyshin@users.noreply.github.com>
2022-08-14 13:31:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
 console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
